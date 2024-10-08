86753e8bf9fd2bc48c97d64ba991cdff57d1702a
serros404 <serros404@gmail.com>
2024-10-08 03:10:02 -0300

    Populando tabela Categoria e Produto

---
+++ APICatalogo/APICatalogo/Migrations/20241008053922_PopulaCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias (Nome, ImagemUrl) VALUES ('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO Categorias (Nome, ImagemUrl) VALUES ('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO Categorias (Nome, ImagemUrl) VALUES ('Sobremesas', 'sobremesas.jpg')");
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20241008055937_RemoveTestCategories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTestCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Categorias WHERE Nome NOT IN ('Lanches', 'Sobremesas', 'Bebidas')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            // Opcionalmente, você pode reverter a remoção aqui
+        }
+
+    }
+}
